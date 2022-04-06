9cc1f74fd2887a6e7a1ff12db144ec5683e4069c
Munit Yadav <munit_yadav@thbs.india.com>
2022-04-06 16:48:15 +0530

    added learnytica

--- learnytica/src/app/app-routing.module.ts
+++ learnytica/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ListContactComponent } from './components/list-contact/list-contact.component';
+import { ListCourseComponent } from './components/list-course/list-course.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ServicesComponent } from './components/services/services.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  { path: 'course', component: ListCourseComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'list-contact', component: ListContactComponent },
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
