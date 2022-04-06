f5d51fd65ea355bd147bb72fc9c436eb8b9014a3
Munit Yadav <munit_yadav@thbs.india.com>
2022-04-06 12:02:59 +0530

    added card in learnytica

---
+++ learnytica/src/app/components/course/course.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  courseTitle = 'Git for begginer';
+  currentDate = new Date();
+
+  courseDetails = {
+    title: 'Dev',
+    price: 100
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
