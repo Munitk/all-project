feea591fd986e5e302909804bd43c1e70c17ef8e
munit <munitkumardbg@gnmail.com>
2022-04-11 12:04:39 +0530

    added material angular

--- learnytica/src/app/app.module.ts
+++ learnytica/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatSliderModule} from '@angular/material/slider';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
